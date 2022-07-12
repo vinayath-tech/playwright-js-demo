48866db25a1007ff159001403f9e219dc061f8ef
gokul <gokul.sridharan@blackcatsolutions.co.uk>
2022-07-12 22:25:17 +0100

    wip code

--- tests/crossbrowsertest.spec.ts
+++
@@ -1,7 +0,0 @@
-import { test } from '@playwright/test';
-
-test('Execute tests in saucelabs',async({page}) => {
-    await page.goto('https://google.com');
-    await page.waitForTimeout(5000);
-});
-
